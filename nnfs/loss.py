7f68af9ea2c7c4e37f897cc62ab27b00b14483e2
Håkon Magne Holmen <haakonholmen@hotmail.com>
2023-08-27 00:07:08 +0200

    Add more metrics and loss functions

--- nnfs/loss.py
+++ nnfs/loss.py
@@ -1,3 +1,6 @@
+import math
+
+
 def mse(y_true, y_pred):
     # Convert inputs to lists of values if they are scalars
     if not hasattr(y_true, '__iter__'):
@@ -18,3 +21,15 @@ def accuracy(y_true, y_pred):
 
     num_items = len(y_true)
     return sum((y_i == pred_i) for y_i, pred_i in zip(y_true, y_pred)) / num_items
+
+
+def cross_entropy(y_true, y_pred):
+    # Convert inputs to lists of values if they are scalars
+    if not hasattr(y_true, '__iter__'):
+        y_true = [y_true]
+    if not hasattr(y_pred, '__iter__'):
+        y_pred = [y_pred]
+
+    num_items = len(y_true)
+    return -sum((y_i * pred_i.log() + (1 - y_i) * math.log(1 - pred_i))
+                for y_i, pred_i in zip(y_true, y_pred)) / num_items
