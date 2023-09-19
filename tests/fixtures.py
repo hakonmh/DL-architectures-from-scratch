fd3bdfeb45584e165e39b0c32b639cd8ad497eb6
Håkon Magne Holmen <haakonholmen@hotmail.com>
2023-09-19 17:28:20 +0200

    Add ValueArray tests

--- tests/fixtures.py
+++ tests/fixtures.py
@@ -1,5 +1,6 @@
 import math
 import torch
+from dlafs import Value
 
 
 def assert_grads_equal_expected(values, expected_grads):
@@ -7,3 +8,33 @@ def assert_grads_equal_expected(values, expected_grads):
         if isinstance(exp_grad, torch.Tensor):
             exp_grad = exp_grad.grad.item()
         assert math.isclose(value.grad, exp_grad)
+
+
+def convert_list_items_to_value(data):
+    """Recursive function to create valuearray from nested list"""
+    if isinstance(data, list):
+        return [convert_list_items_to_value(item) for item in data]
+    else:
+        if not isinstance(data, Value):
+            return Value(data)
+        else:
+            return data
+
+
+def format_index_args_string(indexers_dict):
+    """Convert test setitem/getitem args into strs of format '{num_dims}D, [{index}]'."""
+    ids = []
+    for num_dim, indexers, in indexers_dict.items():
+        for index in indexers:
+            if isinstance(index, tuple):
+                index = index[0]
+            _id = []
+            for i in index:
+                if isinstance(i, slice):
+                    _id.append(f"{i.start}:{i.stop}")
+                else:
+                    _id.append(str(i))
+            _id = f'{num_dim}D, [' + ', '.join(_id) + ']'
+            _id = _id.replace('None', '')
+            ids.append(_id)
+    return ids
---
+++ tests/test_array.py
@@ -0,0 +1,194 @@
+import pytest
+from .fixtures import *
+
+import numpy as np
+from dlafs import ValueArray
+from dlafs import Value as V
+
+
+@pytest.mark.parametrize(
+    'shape, expected',
+    [
+        ((1, ), [V(0), ]),
+        ((3, ), [V(0), V(0), V(0)]),
+        ((1, 1), [[V(0), ]]),
+        ((2, 2), [[V(0), V(0)], [V(0), V(0)]]),
+        ((3, 1, 2), [[[V(0), V(0)], ], [[V(0), V(0)], ], [[V(0), V(0)], ]]),
+        ((1, 2, 1, 2), [[[[V(0), V(0)], ], [[V(0), V(0)], ]]]),
+    ],
+    ids=['(1, )', '(3, )', '(1, 1)', '(2, 2)', '(3, 1, 2)', '(1, 2, 1, 2)']
+)
+def test_valuearray_init(shape, expected):
+    # Act
+    actual = ValueArray(shape)
+    # Assert
+    assert actual.values == expected
+    assert actual.to_numpy().shape == shape
+
+
+@pytest.mark.parametrize(
+    'input_list',
+    [
+        [1, ],
+        [V(1), ],
+        [1.01, 2.02],
+        [[1, ]],
+        [[[[V(1), V(2)], [V(3), V(4)]], [[V(5), V(6)], [V(7), V(8)]]]],
+    ],
+    ids=['single int', 'single value', '1D float', '2D int', '4D value']
+)
+def test_from_list(input_list):
+    # Arrange
+    expected = convert_list_items_to_value(input_list)
+    # Act
+    actual = ValueArray.from_list(input_list)
+    # Assert
+    assert actual.values == expected
+
+
+@pytest.mark.parametrize(
+    'input_list',
+    [
+        [1, ],
+        [1.01, 2.02],
+        [[1, ]],
+        [[[[1, 2], [3, 4]], [[5, 6], [7, 8]]]],
+    ],
+    ids=['single', '1D float', '2D', '4D']
+)
+def test_from_numpy(input_list):
+    # Arrange
+    np_array = np.array(input_list)
+    expected = convert_list_items_to_value(input_list)
+    expected_dtype = 'float' if 'float' in str(np_array.dtype) else 'int'
+    # Act
+    actual = ValueArray.from_numpy(np_array)
+    # Assert
+    assert actual.values == expected
+    assert actual.shape == np_array.shape
+    assert actual.dtype == expected_dtype
+
+
+def test_to_numpy():
+    # Arrange
+    varray = ValueArray((2, 2, 3))
+    expected = np.zeros((2, 2, 3))
+    # Act
+    actual = varray.to_numpy()
+    # Assert
+    assert np.array_equal(actual, expected)
+
+
+TEST_GETITEM_ARGS = {  # num_dims: (index, ...)
+    1: ([0], [[0, 2]], [-1], [slice(1, 3)], [slice(None)]),
+    2: ([3, 2], [0], [[2, -1], 0],
+        [slice(1, 3), slice(1, 3)],
+        [3, slice(None)], [slice(None), 2],
+        [slice(None), slice(None)], [slice(None)]),
+    4: ([0, 1, 2, 3], [0, 1],
+        [[0, 2], 0, 0, 0], [1, 2, [0, 2], 0],
+        [1, slice(1, 3), 0, 0], [0, 1, slice(None)],
+        [slice(None), slice(None), slice(None), slice(None)],
+        [slice(None), slice(None)]),
+}
+
+
+@pytest.mark.parametrize(
+    "num_dims, index",
+    [(n, i) for n, indexer in TEST_GETITEM_ARGS.items() for i in indexer],
+    ids=format_index_args_string(TEST_GETITEM_ARGS)
+)
+def test_getitem(num_dims, index):
+    index = tuple(index)
+    np_array = _create_array(num_dims)
+    varray = ValueArray.from_numpy(np_array)
+
+    expected = np_array[index]
+    expected_shape = expected.shape
+    expected = convert_list_items_to_value(expected.tolist())
+    # Act
+    actual = varray[index]
+    # Assert
+    if isinstance(actual, ValueArray):
+        assert actual.values == expected
+        assert actual.shape == expected_shape
+    else:
+        assert actual == expected
+
+
+TEST_SETITEM_ARGS = {  # num_dims: [(index, values), ...]
+    1: [
+        ([0], 1),
+        ([[0, 2]], [3, 3]),
+        ([slice(1, 3)], [5, 5]),
+        ([slice(None)], [4, 3, 2, 1]),
+    ],
+    2: [
+        ([3, 2], 1),
+        ([0, ], [5, 5, 5, 5]),
+        ([0, [1, 3]], [5, 5]),
+        ([[1, 3], 0], [5, 5]),
+        ([slice(None), 0], [5, 5, 5, 5]),
+        ([slice(1, 3), slice(1, 3)], [[5, 5], [5, 5]]),
+    ],
+    4: [
+        ([0, 1, 2, 3], 1),
+        ([0, 1, 2], [5, 5, 5, 5]),
+        ([[0, 2], 0, 0, 0], [5, 5]),
+        ([1, 2, [0, 2], 0], [5, 5]),
+        ([0, 1, slice(None)], [[5, 5, 5, 5]] * 4),
+        ([slice(None), slice(None), slice(None), slice(None)], ValueArray((4, 4, 4, 4)).to_list()),
+        ([slice(None), slice(None)], ValueArray((4, 4, 4, 4)).to_list()),
+    ]
+}
+
+
+@pytest.mark.parametrize(
+    "num_dims, index, values",
+    ((n, *i) for n, indexer in TEST_SETITEM_ARGS.items() for i in indexer),
+    ids=format_index_args_string(TEST_SETITEM_ARGS)
+)
+def test_setitem(num_dims, index, values):
+    index = tuple(index)
+
+    np_array = _create_array(num_dims)
+    actual = ValueArray.from_numpy(np_array)
+
+    np_array[index] = values
+    expected_shape = np_array.shape
+    expected = convert_list_items_to_value(np_array.tolist())
+    # Act
+    actual[index] = values
+    # Assert
+    if isinstance(actual, ValueArray):
+        assert actual.values == expected
+        assert actual.shape == expected_shape
+    else:
+        assert actual == expected
+
+
+@pytest.mark.parametrize(
+    "index, values, exception",
+    [([1, 1], [-0.1, 0.1], ValueError),
+     ([1, slice(1, 3)], [1, 2, 3], ValueError),
+     ([slice(1, 3), 1], [1, 2, 3], ValueError),
+     ([1, slice(1, 3)], 1, ValueError),
+     ([slice(1, 3), 0], 2, ValueError),
+     ([slice(None), slice(None)], 3, ValueError),
+     ([7, 7], 4, IndexError),
+     ([1, 1], '5', TypeError),
+     ],
+)
+def test_setitem_exeptions(index, values, exception):
+    # Arrange
+    index = tuple(index)
+    np_array = _create_array(num_dims=2)
+    varray = ValueArray.from_numpy(np_array)
+    # Act & Assert
+    with pytest.raises(exception):
+        varray[index] = values
+
+
+def _create_array(num_dims, size=4):
+    shape = [size] * num_dims
+    return np.arange(np.prod(shape)).reshape(shape)
